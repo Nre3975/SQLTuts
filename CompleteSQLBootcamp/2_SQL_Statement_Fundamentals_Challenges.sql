a94e4e9ad0b8d8f0a87bc54af8a7477011b846f9
NRE3975 <30232487+Nre3975@users.noreply.github.com>
2019-10-31 21:58:04 +0000

    Setup SQL Bootcamp Section 2

---
+++ CompleteSQLBootcamp/2_SQL_Statement_Fundamentals_Challenges.sql
@@ -0,0 +1,18 @@
+--  2.12: Challenge: SELECT Task. 
+SELECT first_name, last_name, email
+FROM customer; 
+
+
+-- 2.14: Challenge: SELECT DISTINCT. 
+
+
+
+-- 2.16: Challenge: SELECT WHERE. 
+
+
+
+-- 2.20: Challenge: ORDER BY. 
+
+
+
+-- 2.24: General Challenge 1. 
---
+++ CompleteSQLBootcamp/2_SQL_Statement_Fundamentals_Notes.sql
@@ -0,0 +1,7 @@
+-- 13: SELECT DISTINCT. 
+
+-- Check how many distinct years are in the table. 
+SELECT DISTINCT release_year FROM film;
+
+-- Check how many distinct prices are in the table. 
+SELECT DISTINCT rental_rate FROM film;
