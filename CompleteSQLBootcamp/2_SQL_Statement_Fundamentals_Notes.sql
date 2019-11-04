c902428a9f537559bc4746a3165d131077277e80
NRE3975 <30232487+Nre3975@users.noreply.github.com>
2019-11-04 21:42:41 +0000

    SQL Statement Fundamentals Completed

--- CompleteSQLBootcamp/2_SQL_Statement_Fundamentals_Notes.sql
+++ CompleteSQLBootcamp/2_SQL_Statement_Fundamentals_Notes.sql
@@ -1,9 +1,8 @@
 /* 13: SELECT DISTINCT. */ 
-
--- Check how many distinct years are in the table. 
+-- Check how many distinct years are in the table:
 SELECT DISTINCT release_year FROM film;
 
--- Check how many distinct prices are in the table. 
+-- Check how many distinct prices are in the table:
 SELECT DISTINCT rental_rate FROM film;
 
 ---------------------------------------------------------------------------
@@ -11,14 +10,17 @@ SELECT DISTINCT rental_rate FROM film;
 -- Where: Filter rows returned in SELECT statement. 
 -- Operators: = > < >= <= <> != AND OR. 
 
+-- Find rows where first name and last name match clause: 
 SELECT last_name, first_name 
   FROM customer 
  WHERE first_name = 'Jamie' AND last_name = 'Rice'; 
 
+-- Rows where amount is <= 1 or >= 8: 
 SELECT customer_id, amount, payment_date 
   FROM payment 
  WHERE amount <= 1 OR amount >= 8; 
 
+-- Rows where store id is 1, and address id is more than 5: 
 SELECT * 
   FROM customer 
  WHERE store_id = 1 AND address_id > 5
@@ -26,7 +28,8 @@ SELECT *
 ------------------------------------------------------------------------------------
 /* 17: COUNT */
 -- Count number of rows where condition is true. 
--- Counting a specific column will NOT count null rows. 
+
+-- Counting a specific column will NOT count null rows:
 SELECT COUNT(*) 
   FROM payment; 
 
@@ -37,11 +40,13 @@ SELECT COUNT(DISTINCT amount)
 ------------------------------------------------------------------------------------
 /* 18: Limit. */
 -- Limit the number of rows returned, does not exist in ORACLE SQL. 
+
+-- Return 5 rows only:
 SELECT * 
   FROM customer
  LIMIT 5; 
 
--- FETCH NEXT Does exist in ORACLE and POSTGRES
+-- FETCH NEXT Does exist in ORACLE and POSTGRES:
 SELECT * 
   FROM film 
  FETCH NEXT 5 ROWS ONLY;
@@ -62,48 +67,51 @@ OFFSET 5 ROWS
 -- Sort data by column(s) in ascending or descending order. 
 -- In postgres can also sort by columns not returned in the select statement
 
+-- Order by first names Z-A:
 SELECT first_name
   FROM customer 
  ORDER BY first_name DESC; 
 
+-- Order By first name A-Z, duplicate first names will be orders by last_name Z-A:
 SELECT first_name, last_name
   FROM customer 
  ORDER BY first_name ASC, last_name DESC; 
 
+-- Order by a column not returned: (Can Oracle do this?)
 SELECT email
   FROM customer
  ORDER BY last_name DESC; 
 
 ------------------------------------------------------------------------------------
-/* 21 Between */
+/* 21: Between */
 -- Match a value against a range of values. 
 -- value BETWEEN HIGH AND LOW  == Value >- low AND value <= high; 
 -- NOT BETWEEN to check value is not within a range. 
 
--- Any customer ids who had an amount between 2 values
+-- Any customer ids who had an amount between 2 values:
 SELECT customer_id, amount  
   FROM payment 
  WHERE amount BETWEEN 8.00 AND 9.00
  LIMIT 5; 
 
--- Any customer ids who had an amount not between the same 2 values
+-- Any customer ids who had an amount not between the same 2 values:
 SELECT customer_id, amount  
   FROM payment 
  WHERE amount NOT BETWEEN 8.00 AND 9.00
  LIMIT 5; 
 
--- All orders between 2 dates. 
+-- All orders between 2 dates:
 SELECT payment_date
   FROM payment 
  WHERE payment_date BETWEEN '2007-02-07' AND '2007-02-15'
 
 ------------------------------------------------------------------------------------
-/* 22 In */
+/* 22: In */
 -- Check when a value matches another value in a given list of values or subquery. 
 -- Returns TRUE/FALSE. 
 -- Can reverse with NOT. 
 
--- Find rental information if customer ID is in the list of values: 1, 2. 
+-- Find rental information if customer ID is in the list of values: 1, 2:
 SELECT rental_id, customer_id, return_date
   FROM rental 
  WHERE customer_id IN (1, 2)
@@ -116,7 +124,7 @@ SELECT rental_id, customer_id, return_date
     OR customer_id = 2 
  ORDER BY return_date DESC; */
 
--- Find rental information if customer ID is not in the list of values: 7, 13, 10
+-- Find rental information if customer ID is not in the list of values: 7, 13, 10:
 SELECT rental_id, customer_id, return_date
   FROM rental 
  WHERE customer_id NOT IN (7, 13, 10) 
@@ -128,3 +136,44 @@ SELECT *
  WHERE amount IN (7.99, 8.99); 
 
 ------------------------------------------------------------------------------------
+/* 23: Like */
+-- String/Substring pattern matching. 
+-- % = Wildcard Sequence, _ Single Wildcard Character. 
+-- Can inverse with NOT.
+-- CASE Sensitive: Postgres has iLike which is not case sensitive, ORACLE SQL does 
+-- not have iLike, instead convert to UPPER/LOWER/INITCAP etc before comparing. 
+
+-- Return rows where first name starts with Jen:
+SELECT first_name, last_name 
+  FROM customer
+ WHERE first_name LIKE 'Jen%';
+
+-- Returns rows where name is Jenn and 1 letter after: (Jenny, not Jennie):
+SELECT first_name, last_name 
+  FROM customer
+ WHERE first_name LIKE 'Jenn_';
+
+-- Return rows where name ends in a y:
+SELECT first_name, last_name 
+  FROM customer
+ WHERE first_name LIKE '%y';
+
+-- Return rows where name has 'er' anywhere in the name:
+SELECT first_name, last_name 
+  FROM customer
+ WHERE first_name LIKE '%er%';
+
+-- Names where 'her' are characters 2,3,4. With 1 letter before and any sequence after:
+ SELECT first_name, last_name 
+  FROM customer
+ WHERE first_name LIKE '_her%';
+
+-- Names not like Jen:
+SELECT first_name, last_name 
+  FROM customer
+ WHERE first_name NOT LIKE 'Jen%';
+
+-- ILIKE Example: 
+SELECT first_name, last_name 
+  FROM customer
+ WHERE first_name ILIKE 'BAR%';
