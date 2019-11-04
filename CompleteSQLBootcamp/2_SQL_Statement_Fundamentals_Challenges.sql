7fba1abdc0d612bba0e0c6b4a22412f9d52e5cf7
NRE3975 <30232487+Nre3975@users.noreply.github.com>
2019-11-04 20:43:37 +0000

    Order By Challenge Completed

--- CompleteSQLBootcamp/2_SQL_Statement_Fundamentals_Challenges.sql
+++ CompleteSQLBootcamp/2_SQL_Statement_Fundamentals_Challenges.sql
@@ -1,18 +1,51 @@
 --  2.12: Challenge: SELECT Task. 
+
 SELECT first_name, last_name, email
-FROM customer; 
+  FROM customer; 
 
-
+---------------------------------------------------------------------------
 -- 2.14: Challenge: SELECT DISTINCT. 
 
+-- Check how many distinct years are in the table. 
+SELECT DISTINCT rating 
+  FROM film; 
 
-
+---------------------------------------------------------------------------
 -- 2.16: Challenge: SELECT WHERE. 
 
+-- 1) What's the email for customer with name Nancy Thomas?
+SELECT email 
+  FROM customer 
+ WHERE first_name = 'Nancy' 
+   AND last_name = 'Thomas'
 
+-- 2) What is the description for the movie: 'Outlaw Hanky'?
+SELECT description 
+  FROM film 
+ WHERE title = 'Outlaw Hanky'
 
+-- 3) What is the phone number for person living at 259 Ipoh Drive? 
+SELECT phone 
+  FROM address
+ WHERE address = '259 Ipoh Drive'
+ 
+---------------------------------------------------------------------------
 -- 2.20: Challenge: ORDER BY. 
 
+-- 1) Customer IDs for top 10 payment amounts. 
+SELECT * 
+  FROM payment 
+ ORDER BY amount DESC
+ FETCH FIRST 10 ROWS ONLY; 
 
+SELECT * 
+  FROM payment 
+ ORDER BY amount DESC
+ LIMIT 10;
 
+-- 2) Titles of the movies with film IDs 1-5 
+ SELECT * 
+  FROM film 
+ FETCH FIRST 5 ROWS ONLY;
+---------------------------------------------------------------------------
 -- 2.24: General Challenge 1. 
--- CompleteSQLBootcamp/2_SQL_Statement_Fundamentals_Notes.sql
+++ CompleteSQLBootcamp/2_SQL_Statement_Fundamentals_Notes.sql
@@ -1,7 +1,77 @@
--- 13: SELECT DISTINCT. 
+/* 13: SELECT DISTINCT. */ 
 
 -- Check how many distinct years are in the table. 
 SELECT DISTINCT release_year FROM film;
 
 -- Check how many distinct prices are in the table. 
 SELECT DISTINCT rental_rate FROM film;
+
+---------------------------------------------------------------------------
+/* 15: SELECT WHERE. */ 
+-- Where: Filter rows returned in SELECT statement. 
+-- Operators: = > < >= <= <> != AND OR. 
+
+SELECT last_name, first_name 
+  FROM customer 
+ WHERE first_name = 'Jamie' AND last_name = 'Rice'; 
+
+SELECT customer_id, amount, payment_date 
+  FROM payment 
+ WHERE amount <= 1 OR amount >= 8; 
+
+SELECT * 
+  FROM customer 
+ WHERE store_id = 1 AND address_id > 5
+
+---------------------------------------------------------------------------
+/* 17: COUNT */
+-- Count number of rows where condition is true. 
+-- Counting a specific column will NOT count null rows. 
+SELECT COUNT(*) 
+  FROM payment; 
+
+-- Count unique values in a table: 
+SELECT COUNT(DISTINCT amount)
+  FROM payment; 
+
+---------------------------------------------------------------------------
+/* 18: Limit. */
+-- Limit the number of rows returned, does not exist in ORACLE SQL. 
+SELECT * 
+  FROM customer
+ LIMIT 5; 
+
+-- FETCH NEXT Does exist in ORACLE and POSTGRES
+SELECT * 
+  FROM film 
+ FETCH NEXT 5 ROWS ONLY;
+
+ SELECT * 
+  FROM film 
+ FETCH FIRST 5 ROWS ONLY;
+
+-- Fetch film ID 6 - 10... 
+ SELECT * 
+  FROM film 
+ ORDER BY film_id  
+OFFSET 5 ROWS
+ FETCH FIRST 5 ROWS ONLY;
+
+
+/* 19: Order By. */
+-- Sort data by column(s) in ascending or descending order. 
+-- In postgres can also sort by columns not returned in the select statement
+
+SELECT first_name
+  FROM customer 
+ ORDER BY first_name DESC; 
+
+SELECT first_name, last_name
+  FROM customer 
+ ORDER BY first_name ASC, last_name DESC; 
+
+SELECT email
+  FROM customer
+ ORDER BY last_name DESC; 
+
+ 
