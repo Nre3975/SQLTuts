da8b628d465a25ba2581074163773f27500ebd18
NRE3975 <30232487+Nre3975@users.noreply.github.com>
2019-11-16 12:56:37 +0000

    GROUPBY Challenge Done

--- CompleteSQLBootcamp/3_GROUPBY_Statements_Challenges.sql
+++ CompleteSQLBootcamp/3_GROUPBY_Statements_Challenges.sql
@@ -1,6 +1,20 @@
-/* 25: MIN, MAX, SUM, AVG. */ 
--- Check how many distinct years are in the table:
-SELECT DISTINCT release_year FROM film;
+/* 3.27: Challenge: SELECT Task. */
 
--- Check how many distinct prices are in the table:
-SELECT DISTINCT rental_rate FROM film;
+--1) How many payments did each staff memeber handle? What was the total amount processed by each
+SELECT P.STAFF_ID, S.FIRST_NAME || ' ' || S.LAST_NAME AS NAME, COUNT(P.AMOUNT) AS PAYMENTS_HANDLED, SUM(P.AMOUNT) TOTAL_AMOUNT
+  FROM PAYMENT P, STAFF S
+ WHERE p.staff_id = S.staff_id
+ GROUP BY P.STAFF_ID,  S.FIRST_NAME || ' ' || S.LAST_NAME; 
+ 
+ --2) What is the average replacement cost of a movie broken down by rating? 
+ SELECT rating, ROUND(AVG(replacement_cost), 2)
+  FROM film
+ GROUP BY rating; 
+
+--3) What are the customer ID's of the top 5 spenders? 
+SELECT p.customer_id, c.first_name || ' ' || c.last_name AS NAME, SUM(amount) AS TOTAL
+  FROM payment P, customer C 
+ WHERE p.customer_id = c.customer_id 
+ GROUP BY p.customer_id, c.first_name || ' ' || c.last_name
+ ORDER BY sum(amount) DESC
+ FETCH NEXT 5 ROWS ONLY; 
