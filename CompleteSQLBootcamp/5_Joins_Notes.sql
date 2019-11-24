7a1eb0c45c6eef3be6b6154d13fc44d288ebb59a
NRE3975 <30232487+Nre3975@users.noreply.github.com>
2019-11-24 23:30:34 +0000

    38: Overview Of Join Types: Complete

--- CompleteSQLBootcamp/5_Joins_Notes.sql
+++ CompleteSQLBootcamp/5_Joins_Notes.sql
@@ -89,4 +89,95 @@ SELECT f.title, l.name AS movie_language
   FROM film f, language l
  WHERE f.language_id  = l.language_id;
 
+------------------------------------------------------------------------------------
+/* 38: Overview Of Join Types  */ 
 
+/*  Table A                 Table B 
+    ID | NAME               ID | NAME 
+    1  | Pirate             1  | Rutabaga 
+    2  | Monkey             2  | Pirate  
+    3  | Ninja              3  | Darth Vader
+    4  | Spaghetti          4  | Ninja 
+*/ 
+
+-- Inner join:
+-- Returns only records which show in both table A and table B. 
+SELECT * 
+  FROM TableA 
+ INNER JOIN TableB ON TableA.name = TableB.name; 
+
+/*
+1  | Pirate             2  | Pirate
+3  | Ninja              4  | Ninja
+*/ 
+
+-- Full Outer Join. 
+-- Returns set of all records in A and B with matching records from both sides 
+-- where available. If no match, missing side will contain a null. 
+SELECT * 
+  FROM TableA
+  FULL OUTER JOIN TableB ON TableA.name = TableB.name;
+
+/*
+1    | Pirate             2    | Pirate
+2    | Monkey             NULL | NULL
+3    | Ninja              4    | Ninja
+4    | Spaghetti          NULL | NULL
+NULL | NULL               1    | Rutabaga 
+NULL | NULL               3    | Darth Vader
+*/ 
+
+-- Left Outer Join 
+-- Returns all records from table A, with the matching records if available from 
+-- Table B, If nothing available in Table B, returns NULL.
+-- Doesn't bring back anything in Table B not in Table A. 
+SELECT * 
+  FROM TableA
+  LEFT OUTER JOIN TableB ON TableA.name = TableB.name;
+
+/*
+1    | Pirate             2    | Pirate
+2    | Monkey             NULL | NULL
+3    | Ninja              4    | Ninja
+4    | Spaghetti          NULL | NULL
+*/
+
+-- Right Outer Join. 
+-- Same as Left Outer Join but reversed. 
+SELECT * 
+  FROM TableA
+ RIGHT OUTER JOIN TableB ON TableA.name = TableB.name;
+
+/*
+NULL | NULL               1    | Rutabaga 
+1    | Pirate             2    | Pirate
+NULL | NULL               3    | Darth Vader
+3    | Ninja              4    | Ninja
+*/ 
+
+--Left Outer Join with NULL WHERE. 
+--Return records in Table A which are not in Table B. 
+--Can be reversed using RIGHT instead of LEFT. 
+SELECT * 
+  FROM TableA
+  LEFT OUTER JOIN TableB ON TableA.name = TableB.name
+ WHERE TableB.ID IS NULL;
+
+/*
+2    | Monkey             NULL | NULL
+4    | Spaghetti          NULL | NULL
+*/
+
+--FULL Outer join with NULL WHERE clause. 
+--RETURN results unique to Table A or Table B, ignore any in both. 
+SELECT * 
+  FROM TableA 
+  FULL OUTER JOIN TableB on TableA.name = TableB.name 
+ WHERE TableA.ID IS NULL OR TableB.ID IS NULL OR 
+
+ /*
+2    | Monkey             NULL | NULL
+4    | Spaghetti          NULL | NULL
+NULL | NULL               1    | Rutabaga 
+NULL | NULL               3    | Darth Vader
+*/ 
