ec3dd75ce820c22341b9ff23e89ad4cfbcfefd3d
NRE3975 <30232487+Nre3975@users.noreply.github.com>
2019-11-24 22:29:09 +0000

    37: Inner Joins Examples. Complete

--- CompleteSQLBootcamp/5_Joins_Notes.sql
+++ CompleteSQLBootcamp/5_Joins_Notes.sql
@@ -18,7 +18,8 @@ SELECT a.pka, a.columnname1, b.fka, b.columnname
 
 -- DBMS: For each row in Table A, see if a row in Table B matches condition. 
 
--- Inner Join: Returns rows in Table A that have corresponding rows in Table B.. 
+-- Inner Join: Returns rows in Table A that have corresponding rows in Table B.
+-- Using ANSI standard Inner Join can be expressed as just JOIN. 
 
 -- Example: Join customer and payment tables. (Ansii Join Style)
 SELECT c.customer_id, c.first_name, c.last_name, c.email,
@@ -45,6 +46,47 @@ SELECT c.customer_id, c.first_name, c.last_name, c.email,
   FROM customer c
  INNER JOIN payment p ON c.customer_id = p.customer_id
  WHERE c.customer_id = 2;
- 
+
 ------------------------------------------------------------------------------------
+/* 37: Examples of Inner Join  */ 
+-- ANSI Join
+SELECT p.payment_id, p.amount, 
+		s.first_name, s.last_name 
+  FROM payment p 
+ INNER JOIN staff s ON p.staff_id = s.staff_id;
+ 
+ -- Non-Ansi Join
+SELECT p.payment_id, p.amount, 
+		s.first_name, s.last_name 
+  FROM payment p, staff s
+ WHERE p.staff_id = s.staff_id; 
+
+-- How many titles of each film are at store 1: 
+-- ANSI Join
+SELECT f.title, COUNT(f.title) AS "Copies_At_Store"
+  FROM inventory i
+ INNER JOIN film f ON i.film_id = f.film_id
+ WHERE store_id = 1
+ GROUP BY f.title
+ ORDER BY f.title;
+ 
+-- Non-Ansi Join
+SELECT f.title, COUNT(f.title) AS "Copies_At_Store"
+  FROM inventory i, film f 
+ WHERE i.film_id = f.film_id
+   AND store_id = 1
+ GROUP BY f.title
+ ORDER BY f.title;
+
+-- Languages attached to the films: 
+-- ANSI Join
+SELECT f.title, l.name AS movie_language
+  FROM film f
+ INNER JOIN language l ON f.language_id  = l.language_id;
+ 
+-- Non-ANSI Join
+SELECT f.title, l.name AS movie_language
+  FROM film f, language l
+ WHERE f.language_id  = l.language_id;
+
 
