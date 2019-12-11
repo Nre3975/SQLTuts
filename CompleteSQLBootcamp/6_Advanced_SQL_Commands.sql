6ef50e3fa14bd093e6f08c2baf8a2611df5afb3f
Nre3975 <30232487+Nre3975@users.noreply.github.com>
2019-12-11 22:46:07 +0000

    47: Self-Join

--- CompleteSQLBootcamp/6_Advanced_SQL_Commands.sql
+++ CompleteSQLBootcamp/6_Advanced_SQL_Commands.sql
@@ -95,3 +95,74 @@ SELECT film_id, title
   					  FROM rental r 
  					 INNER JOIN inventory i ON r.inventory_id = i.inventory_id
  					 WHERE r.return_date BETWEEN '2005-05-29' AND '2005-05-30')
+
+----------------------------------------------------------------------------------------------------------
+/* 47: Self Join. */ 
+-- Join a table to itself. 
+-- Combine rows with other rows from the same table
+-- Use table alias to distinguish between the two versions of the table. 
+
+/* Example Table: Which emploees are from the same location as Joe.
+Employee_Name  |  Employee_location 
+Joe            |  New York
+Sunil          |  India
+Alex           |  Russia
+Albert         |  Canada
+Jack           |  New York
+*/
+
+-- Create table:
+CREATE TABLE employee (
+	employee_name VARCHAR(100),
+	employee_location VARCHAR(100)
+);
+
+INSERT INTO employee VALUES('Joe', 'New York');
+INSERT INTO employee VALUES('Sunil', 'India');
+INSERT INTO employee VALUES('Alex', 'Russia');
+INSERT INTO employee VALUES('Albert', 'Canada');
+INSERT INTO employee VALUES('Jack', 'New York');
+
+SELECT * FROM employee;
+
+--NB: Easier ways to do this, but using as example: 
+-- SubQuery Method.
+SELECT employee_name 
+  FROM employee 
+ WHERE employee_location IN (SELECT employee_location
+							    FROM employee 
+							   WHERE employee_name = 'Joe');
+
+-- Self Join (More Efficient)
+SELECT e1.employee_name 
+  FROM employee AS e1, employee AS e2 
+ WHERE e1.employee_location = e2.employee_location 
+   AND e2.employee_name = 'Joe';
+
+-- Doesn't need join syntax. 
+-- More efficient than a subquery. 
+
+-- Will create 2 versions of the table, E1, and E2. 
+-- Query result looks like:
+/*
+e1.employee_name | e1.employee_location | e2.employee_name | e2.employee_location    
+Joe              | New York             |	Joe              | New York
+Jack             | New York             | Joe              | New York
+*/
+
+-- Customer Table Example: 
+-- Find all customers who have a last_name matching another customers first name: 
+SELECT c1.customer_id, c1.first_name, c1.last_name, c2.customer_id, c2.first_name, c2.last_name 
+  FROM customer AS c1, customer AS c2 
+ WHERE c1.first_name = c2.last_name;
+
+-- With Join Syntax: 
+SELECT c1.customer_id, c1.first_name, c1.last_name, c2.customer_id, c2.first_name, c2.last_name 
+  FROM customer AS c1 
+  JOIN customer AS c2 ON c1.first_name = c2.last_name;
+
+-- Can also use left outer joins if needed, would be useless here...Would show people who 
+-- don't have a matching first name as a C2 last name.  
+SELECT c1.customer_id, c1.first_name, c1.last_name, c2.customer_id, c2.first_name, c2.last_name 
+  FROM customer AS c1 
+  LEFT JOIN customer AS c2 ON c1.first_name = c2.last_name
